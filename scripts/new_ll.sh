5a6e534459c07a9610232194e3d166480a90e19c
Spartak <testwarfase@mail.ru>
2020-12-05 14:45:47 +0300

    1.11 ll sorted by d, f, l, c, b (added -maxdepth 1)

--- scripts/new_ll.sh
+++ scripts/new_ll.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "1. Каталоги:"
-find . -type d -ls
+find . -maxdepth 1 -type d -ls
 
 echo "2. Обычные файлы:"
-find . -type f -ls
+find . -maxdepth 1 -type f -ls
 
 echo "3. Символьные ссылки:"
-find . -type l -ls 
+find . -maxdepth 1 -type l -ls 
 
 echo "4. Символьные устройства:"
-find . -type c -ls
+find . -maxdepth 1 -type c -ls
 
 echo "5. Блочные устройства:"
-find . -type b -ls
+find . -maxdepth 1 -type b -ls
 
